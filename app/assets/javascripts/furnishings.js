ec4b3d726e59012d3395f3587c95f3a0763b426d
Ben Emson <ben@emson.co.uk>
2013-02-23 00:23:46 +0000

    added furnishings asset

--- app/assets/javascripts/furnishings.js
+++ app/assets/javascripts/furnishings.js
@@ -10,7 +10,5 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery.nivo.slider.js
-//= require jquery.jqzoom-core-pack.js
-//= require jquery.lightbox.js
-//= require main.js
+//= require ./furnishings/jquery.jqzoom-core-pack.js
+//= require ./furnishings/jquery.lightbox.js
