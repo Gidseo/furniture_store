1269b79bb974eae5708b6ab112e90a6381f3df4a
Ben Emson <ben@emson.co.uk>
2013-02-23 01:32:47 +0000

    set http protocol for redirect

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -14,11 +14,10 @@ class ContactController < ApplicationController
     if @visitor.save!
       sent = InterestMailer.interest_email(@visitor).deliver
       flash[:notice] = "Thank you, your interest have been registered."
-      redirect_to :action => 'index'
     else
       flash[:notice] = "Unfortunately there were a problem registering your interest, please call the number on the web site. Thank you."
-      redirect_to :action => 'index'
     end
+    redirect_to :action => 'index', :protocol => 'http://'
   end
 
 end
