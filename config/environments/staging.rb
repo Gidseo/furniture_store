97424765a8cbeac9dc69f11b512ca5cab6d597dc
Ben Emson <ben@emson.co.uk>
2013-02-23 01:23:54 +0000

    set email redirect to http not https

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -67,6 +67,7 @@ FurnitureStore::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   #Mail settings:
+  config.action_mailer.default_url_options = {:protocol => 'http'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "email-smtp.us-east-1.amazonaws.com",
